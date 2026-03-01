a98d138cad040ec45c98f1e095f3817c6ec5d854
mtsioni <thalia.tsioni@gmail.com>
2026-03-01 16:36:29 +0100

    feat: add Name property and parameter to Player.cs

--- HW1/GridGame/Models/Player.cs
+++ HW1/GridGame/Models/Player.cs
@@ -4,14 +4,16 @@ public class Player
 {
     public uint PlayerID {get; set;} //id of a player, same number as the value of a cell of a grid that belong to the player
     public string PlayerColor {get; set;} //player color, in hex?
+    public string Name {get; set;} // added the name property
     public bool CanPlay {get; set;} // true - had a move, false - out of moves
     public uint? SelectedRow {get; set;} // row of a cell player wants to fill
     public uint? SelectedColumn {get; set;} // column of a cell player wants to fill
     public uint? MovesLeft {get; set;}
-    public Player(uint id, string color) // constructor
+    public Player(uint id, string color, string name) // constructor
     {
         PlayerID = id;
         PlayerColor = color;
+        Name = name;
         CanPlay = true;
         SelectedRow = null;
         SelectedColumn = null;
