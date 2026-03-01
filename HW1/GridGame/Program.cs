c292ae7cb31f8b27237987f788e9760cbbfcbc29
mtsioni <thalia.tsioni@gmail.com>
2026-03-01 02:31:01 +0100

    add Program.cs with Avalonia AppBuilder entry point

--- HW1/GridGame/Program.cs
+++ HW1/GridGame/Program.cs
@@ -1,8 +1,17 @@
-﻿namespace logiclayer;
-public class Program
+﻿using Avalonia;
+
+namespace GridGame;
+
+class Program
 {
-    private static void Main()
-    {
-        
-    }
+    [STAThread] //to run on a single thread
+    public static void Main(string[] args) =>
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args); // opens the window and starts the evet loop aka application
+
+    public static AppBuilder BuildAvaloniaApp() =>
+        AppBuilder
+            .Configure<App>() //tells Avalonia that "App" is the starting point
+            .UsePlatformDetect() // detects the OS and uses the correct renderer
+            .WithInterFont() // loads the default font
+            .LogToTrace(); //sends interal Avalonia logs to debug output //for debugging
 }
